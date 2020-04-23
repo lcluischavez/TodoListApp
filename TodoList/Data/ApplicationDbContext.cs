199d8e2c71e3cecf88c9004a742b21f5c2dde942
lcluischavez <57668579+lcluischavez@users.noreply.github.com>
2020-04-22 23:49:08 -0500

    Initial

---
+++ TodoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {   
+        }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+    }
+}
---
+++ TodoList/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TodoItem
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int TodoStatusId { get; set; }
+        public TodoStatus TodoStatus { get; set; }
+        [Required]
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
